2b1f7631c917c3591175ccd08dc376aa9dfacba6
doorvasulu-pennaluru <doorvasulupennaluru@gmail.com>
2026-02-08 15:41:49 +0530

    username and password

---
+++ 06-variable.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+echo "Please Enter Username: "
+read USER_NAME
+
+echo " Username is $USER_NAME "
+
+echo " Please enter Your Password: "
+read -s PASSWORD
+
+echo " Password is $PASSWORD "
