a8ef8c069245b3ea850b5b213b5ee0a86dd6fbd6
doorvasulu-pennaluru <doorvasulupennaluru@gmail.com>
2026-02-08 15:32:40 +0530

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+ #!bin/bash
+
+PERSION1=Trump #no space between persion1 and =
+PERSION2=Kim
+echo "$PERSION1:: hello $PERSION2, how are you"
+echo "$PERSION2:: hi $PERSION1 i am fine and thanks for asking, how are you doing"
+echo "$PERSION1:: i am fine, what are you learning today"
+echo "$PERSION2:: i am learning shell script"
