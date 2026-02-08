2877791db3e70b61666c3faf9a735c266fd4ba13
doorvasulu-pennaluru <doorvasulupennaluru@gmail.com>
2026-02-08 14:38:44 +0530

    Add 03-conversation.sh

---
+++ 03-conversation.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "suresh:: hello ramesh, how are you"
+echo "ramesh:: hi suresh i am fine and thanks for asking, how are you doing"
+echo "suresh:: i am fine, what are you learning today"
+echo "ramesh:: i am learning shell script"
